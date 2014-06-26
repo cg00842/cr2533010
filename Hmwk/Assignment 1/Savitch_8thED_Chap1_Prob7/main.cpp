f0be6aa49cc1c2b09647c0623e4ff65674f6b443
cg00842 <cesarrivera319@gmail.com>
2014-06-26 14:00:57 -0700

    Signed-off-by: cg00842 <cesarrivera319@gmail.com>

---
+++ Hmwk/Assignment 1/Savitch_8thED_Chap1_Prob7/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Cesar Rivera-Garcia
+ * Created on June 26, 2014, 1:37 PM
+ * Purpose:Write a program that prints "CS!" in large letters with "*" border
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Execution
+int main(int argc, char** argv)
+{
+    cout<<"*****************************************\n";
+    cout<<"     ccc             ssss          !!\n";
+    cout<<"    c   c          s     s         !!\n";
+    cout<<"   c              s                !!\n";
+    cout<<"  c                s               !!\n";
+    cout<<"  c                 ssss           !!\n";
+    cout<<"  c                      s         !!\n";
+    cout<<"   c                      s        !!\n";
+    cout<<"    c   c          s     s           \n";
+    cout<<"     ccc            ssss           00\n";    
+    cout<<"*****************************************\n";
+    cout<<"     Computer Science is Cool Stuff!!!\n";
+    
+    return 0;
+}
+
